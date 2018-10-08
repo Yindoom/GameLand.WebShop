31dc5180e1d88e375e1f80e24d7add5f3780c6ec
SamPerCaparros <samu1667@easv365.dk>
2018-10-08 13:56:21 +0200

    Implement interface with no code

---
+++ Infrastructure.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShop.Core.Domain;
+using WebShop.Core.Entity;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Customer CreateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer DeleteCustomer(int idCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer ReadCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer UpdateCustomer(Customer customerToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShop.Core.Domain;
+using WebShop.Core.Entity;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Product CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product DeleteProduct(int idProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product ReadProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(Product productToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
