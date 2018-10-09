ade4033b979d9084b147d000e3292b43feafbe2e
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-10-09 18:12:31 +0200

    COntrollerFUnctionality

--- GameLand.WebShop/Program.cs
+++ GameLand.WebShop/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace GameLand.WebShop
@@ -14,7 +16,16 @@ namespace GameLand.WebShop
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var ctx = scope.ServiceProvider.GetService<WebShopDbContext>();
+                DBSeed.SeedDB(ctx);
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- Infrastructure.Data/DBSeed.cs
+++ Infrastructure.Data/DBSeed.cs
@@ -1,7 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using WebShop.Core.Entity;
+
 namespace Infrastructure.Data
 {
     public class DBSeed
     {
-        
+        public static void SeedDB(WebShopDbContext ctx)
+        {
+            ctx.Database.EnsureCreated();
+            if (!ctx.Products.Any() && !ctx.Customers.Any()
+                                    && !ctx.Orders.Any())
+            {
+                ctx.Products.Add(new Product()
+                {
+                    Stock = 5,
+                    Price = 500,
+                    Console = "PS4",
+                    Title = "Spooperman",
+                    ReleaseDate = DateTime.Now
+                });
+                ctx.Products.Add(new Product()
+                {
+                    Stock = 5,
+                    Price = 500,
+                    Console = "PS4",
+                    Title = "God Of War",
+                    ReleaseDate = DateTime.Now
+                });
+                ctx.Products.Add(new Product()
+                {
+                    Stock = 5,
+                    Price = 500,
+                    Console = "PS4",
+                    Title = "Last of Us",
+                    ReleaseDate = DateTime.Now
+                });
+                ctx.Customers.Add(new Customer()
+                {
+                    Address = "Stormgade 6",
+                    Email = "yindoom@hotmail.com",
+                    Name = "Bastian",
+                    PreferredConsole = "PS4"
+                });
+                ctx.Customers.Add(new Customer()
+                {
+                    Address = "Stormgade 6",
+                    Email = "fabio@hotmail.com",
+                    Name = "Fabio",
+                    PreferredConsole = "PC"
+                });
+                ctx.Customers.Add(new Customer()
+                {
+                    Address = "Stormgade 6",
+                    Email = "hotboy@hotmail.com",
+                    Name = "AHotBoy",
+                    PreferredConsole = "XBOX69"
+                });
+               /* ctx.Orders.Add(new Order()
+                {
+                    products = new List<Product>()
+                    {
+                        new Product() {Id = 0},
+                        new Product() {Id = 1}
+                    },
+                    customer = new Customer() {Id = 0},
+                    Price = 1000,
+                });
+                ctx.Orders.Add(new Order()
+                {
+                    products = new List<Product>()
+                    {
+                        new Product() {Id = 0},
+                        new Product() {Id = 2}
+                    },
+                    customer = new Customer() {Id = 1},
+                    Price = 1000,
+                });
+                ctx.Orders.Add(new Order()
+                {
+                    products = new List<Product>()
+                    {
+                        new Product() {Id = 2},
+                        new Product() {Id = 1}
+                    },
+                    customer = new Customer() {Id = 2},
+                    Price = 1000,
+                });*/
+                ctx.SaveChanges();
+            }
+        }
     }
 }
