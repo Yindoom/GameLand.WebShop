03310da88d8efa33b6045a0af58389c870ad0788
SamPerCaparros <samu1667@easv365.dk>
2018-10-09 10:41:24 +0200

    Customer Repository

--- WebShop.Core/Domain/ICustomerRepository.cs
+++ WebShop.Core/Domain/ICustomerRepository.cs
@@ -8,8 +8,9 @@ namespace WebShop.Core.Domain
     public interface ICustomerRepository
     {
 
+        Customer ReadCustomerByID(int id);
         Customer CreateCustomer(Customer customer);
-        Customer ReadCustomer(int id);
+        IEnumerable<Customer> ReadCustomer();
         Customer UpdateCustomer(Customer customerToUpdate);
         Customer DeleteCustomer(int idCustomer);
     }
