e351d5facde29e3e4a0943df715d762315196729
SamPerCaparros <samu1667@easv365.dk>
2018-10-11 12:00:47 +0200

    comment out last name

--- WebShop.Core.Entity/Customer.cs
+++ WebShop.Core.Entity/Customer.cs
@@ -6,7 +6,7 @@ namespace WebShop.Core.Entity
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string LastName { get; set; }
+        //public string LastName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string PreferredConsole { get; set; }
